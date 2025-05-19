c9d2f1916337eed378c3f5daa860bdd76ae63ad7
Divine Elikplim Abah <68868874+deeveli@users.noreply.github.com>
2025-05-19 10:28:02 +0000

    feat: initial commit with starter template

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,11 @@
+import Features from './components/features';
+import Hero from './components/hero';
+import OpenSource from './components/open-source';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
